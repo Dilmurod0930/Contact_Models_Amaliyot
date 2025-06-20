019bd905b161d90629e5c579988203f06ea9d4f1
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-20 20:24:19 +0500

    detail  page  tayyor.  Ishladi  Orqa  sahifaga  ham  qaytadi.

--- contact_app/urls.py
+++ contact_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from  .views import base, contact_lst
+from  .views import base, contact_lst, contact_detail
 
 urlpatterns = [
     path('#', base,name='base'),
     path('', contact_lst, name='contact_list'),
+    path('contact/<int:id>',contact_detail, name='contact_detail'),
 ]
