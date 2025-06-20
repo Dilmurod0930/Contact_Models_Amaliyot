75dd30af417e64770dcd5d29b3d2a44c2038b727
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-20 20:14:51 +0500

    Ditale  Kantakt  malumoti  sahifasi  ham  tayyor

--- contact_app/views.py
+++ contact_app/views.py
@@ -12,3 +12,9 @@ def  contact_lst(request):
     contacts = Contacts.objects.all()
     context = {'contacts': contacts}
     return render(request, 'contact/list.html', context = context)
+
+
+def  contact_detail(request, id):
+    contact = Contacts.objects.get(id=id)
+    context = {'contact': contact}
+    return render(request, 'contact/detail.html', context = context)
